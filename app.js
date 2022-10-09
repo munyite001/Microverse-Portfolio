665895085f1b0242a8164173b3274d59c7e5a301
munyite001 <emunyite@gmail.com>
2022-10-09 14:59:21 +0300

    Fix Linter Errors

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 //  Get the open and close menu btns
-const open_btn = document.getElementById('mobile-menu-btn');
-const close_btn = document.getElementById('close-btn');
+const openBnt = document.getElementById('mobile-menu-btn');
+const closeBtn = document.getElementById('close-btn');
 
 //  Then we also get the mobile menu
-const mobile_menu = document.querySelector('.mobile-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-//  Ten we also need to grab the mobile menu links 
+//  Ten we also need to grab the mobile menu links
 //  so as to close the mobile menu, when a link is clicked
-const menu_links = document.querySelectorAll('.menu-link');
+const menuLinks = document.querySelectorAll('.menu-link');
 
 //  Add an event listener for the menu open btn
-open_btn.addEventListener('click', () => {
-    mobile_menu.classList.add('show-mobile-menu');
-})
+openBnt.addEventListener('click', () => {
+  mobileMenu.classList.add('show-mobile-menu');
+});
 
 //  Add an event listener for the close open btn
-close_btn.addEventListener('click', () => {
-    mobile_menu.classList.remove('show-mobile-menu');
-})
+closeBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('show-mobile-menu');
+});
 
 //  Add an event listener for the menu links in order
 // to close the mobile menu when a link is clicked
-menu_links.forEach(link => {
-    link.addEventListener('click',() => {
-        mobile_menu.classList.remove('show-mobile-menu');
-    })
-})
+menuLinks.forEach(link, () => {
+  link.addEventListener('click',() => {
+    mobileMenu.classList.remove('show-mobile-menu');
+  });
+});
