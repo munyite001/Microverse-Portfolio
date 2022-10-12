93357d5db91ff2ba29bf7ee6ca9c4b1cccd4a3f1
munyite001 <emunyite@gmail.com>
2022-10-12 17:26:09 +0300

    Fix Linter Errors

--- app.js
+++ app.js
@@ -173,7 +173,7 @@ form.addEventListener('submit', (e) => {
   const correctInput = emailInput.toLowerCase();
 
   if (emailInput === correctInput) {
-    form.submit(); 
+    form.submit();
   } else {
     error.style.display = 'block';
     form.elements.email.style.border = '1px solid red';
